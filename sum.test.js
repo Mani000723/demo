5f96bacff5018bced12c1d06aa1ba88aa4df02e4
Manideep0007 <mukkamanideep38@gmail.com>
2025-08-08 16:50:05 +0530

    rectified test file

--- sum.test.js
+++ sum.test.js
@@ -2,7 +2,7 @@ import sum from "./sum.js";
 
 describe("test for sum func", () => {
   test("adds 2+2 equal to 4", () => {
-    expect(sum(2, 2)).toBe(3);
+    expect(sum(2, 2)).toBe(4);
   });
   test("adds 3+2 equal to 5", () => {
     expect(sum(3, 2)).toBe(5);
